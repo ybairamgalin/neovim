ce71ead0214e8be7b08f518cfb352fae61600e7f
Iaroslav Bairamgalin <i.bairamgalin@vk.team>
2024-10-04 14:56:09 +0300

    add parenthesis closing

--- init.lua
+++ init.lua
@@ -190,6 +190,13 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Custom keymap
+vim.keymap.set('n', '<leader>e', vim.cmd.Ex, { desc = 'Go to explorer' })
+
+vim.keymap.set('i', '{', '{}<Esc>i', { noremap = true })
+vim.keymap.set('i', '(', '()<Esc>i', { noremap = true })
+vim.keymap.set('i', '[', '[]<Esc>i', { noremap = true })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
